b4c59b29add73da20e123580170d5f3624518899
bhuvan malik <bhuvanmalik007@gmail.com>
2018-09-03 16:50:35 -0400

    converted answer to first occurrence of series

--- lib/project.ex
+++ lib/project.ex
@@ -2,10 +2,9 @@ import Integer
 
 defmodule Project do
   def testing(n, k) do
-    IO.puts("hey now")
     Enum.reduce(1..n, [], fn x, acc ->
       sqRoot = :math.sqrt((k - 1) * k * (2 * k - 1) / 6 + k * (x * x) + k * (k - 1) * x)
-      (sqRoot - trunc(sqRoot) == 0 && acc ++ [x]) || acc
+      (sqRoot - trunc(sqRoot) == 0 && Process.exit(self, [x])) || acc
     end)
   end
 
@@ -20,7 +19,13 @@ defmodule Project do
     IO.puts("\nCase4: " <> "#{inspect(case4)}")
     case5 = onlyTwoThreeCase(k)
     IO.puts("\nCase5: " <> "#{inspect(case5)}")
-    IO.puts("\n\nFINAL ANSWER: " <> "#{inspect(((case1 || case2 || case3 || case4 ||case5) && "no solution") || (testing(n, k)))}")
+
+    IO.puts(
+      "\n\nFINAL ANSWER: " <>
+        "#{
+          inspect(((case1 || case2 || case3 || case4 || case5) && "no solution") || testing(n, k))
+        }"
+    )
   end
 
   def eightLambda(k) do
@@ -33,81 +38,71 @@ defmodule Project do
 
   def onlyTwoThreeCase(k) do
     primeFactors = factorize(k, 2, [])
-    IO.puts("prime factors: " <> "#{inspect(primeFactors)}")
+    # IO.puts("prime factors: " <> "#{inspect(primeFactors)}")
     unique = Enum.uniq(primeFactors)
-    IO.puts("uniques: " <> "#{inspect(unique)}")
+    # IO.puts("uniques: " <> "#{inspect(unique)}")
     ofForm = Enum.filter(unique, fn x -> x == 2 || x == 3 end)
-    IO.puts("of form: " <> "#{inspect(ofForm)}")
-
+    # IO.puts("of form: " <> "#{inspect(ofForm)}")
     if(ofForm == unique) do
       powers =
         Enum.reduce(ofForm, [], fn x, acc ->
           acc ++ [{x, Enum.count(primeFactors, fn t -> t == x end)}]
         end)
 
-      IO.puts("powers: " <> "#{inspect(powers)}")
-
+      # IO.puts("powers: " <> "#{inspect(powers)}")
       powersOnly =
         Enum.reduce(ofForm, [], fn x, acc ->
           acc ++ [Enum.count(primeFactors, fn t -> t == x end)]
         end)
 
-      IO.puts("\n\npowersOnly: " <> "#{inspect(powersOnly)}")
-
-      Enum.any?(powersOnly, fn power -> Integer.is_even(power) end) ||
-        (k == 2 && false)
+      # IO.puts("\n\npowersOnly: " <> "#{inspect(powersOnly)}")
+      Enum.any?(powersOnly, fn power -> Integer.is_even(power) end) || (k == 2 && false)
     end
   end
 
   @spec twelveLambdaCase(pos_integer) :: [pos_integer]
   def twelveLambdaCase(k) do
     primeFactors = factorize(k, 2, [])
-    IO.puts("prime factors: " <> "#{inspect(primeFactors)}")
+    # IO.puts("prime factors: " <> "#{inspect(primeFactors)}")
     unique = Enum.uniq(primeFactors)
-    IO.puts("uniques: " <> "#{inspect(unique)}")
+    # IO.puts("uniques: " <> "#{inspect(unique)}")
     ofForm = Enum.filter(unique, fn x -> rem(x, 12) == 5 or rem(x, 12) == 7 end)
-    IO.puts("of form: " <> "#{inspect(ofForm)}")
-
+    # IO.puts("of form: " <> "#{inspect(ofForm)}")
     powers =
       Enum.reduce(ofForm, [], fn x, acc ->
         acc ++ [{x, Enum.count(primeFactors, fn t -> t == x end)}]
       end)
 
-    IO.puts("powers: " <> "#{inspect(powers)}")
-
+    # IO.puts("powers: " <> "#{inspect(powers)}")
     powersOnly =
       Enum.reduce(ofForm, [], fn x, acc ->
         acc ++ [Enum.count(primeFactors, fn t -> t == x end)]
       end)
 
-    IO.puts("powersOnly: " <> "#{inspect(powersOnly)}")
+    # IO.puts("powersOnly: " <> "#{inspect(powersOnly)}")
     Enum.any?(powersOnly, fn power -> Integer.is_odd(power) end)
   end
 
   @spec evenTwoThreePowerCase(pos_integer) :: [pos_integer]
   def evenTwoThreePowerCase(k) do
-    IO.puts("inside evenTwoThreePowerCase")
     primeFactors = factorize(k, 2, [])
-    IO.puts("prime factors: " <> "#{inspect(primeFactors)}")
+    # IO.puts("prime factors: " <> "#{inspect(primeFactors)}")
     unique = Enum.uniq(primeFactors)
-    IO.puts("uniques: " <> "#{inspect(unique)}")
-
+    # IO.puts("uniques: " <> "#{inspect(unique)}")
     ofForm = Enum.filter(unique, fn x -> x == 2 || x == 3 end)
-    IO.puts("of form: " <> "#{inspect(ofForm)}")
-
+    # IO.puts("of form: " <> "#{inspect(ofForm)}")
     powers =
       Enum.reduce(ofForm, [], fn x, acc ->
         acc ++ [{x, Enum.count(primeFactors, fn t -> t == x end)}]
       end)
 
-    IO.puts("powers: " <> "#{inspect(powers)}")
-
+    # IO.puts("powers: " <> "#{inspect(powers)}")
     powersOnly =
       Enum.reduce(ofForm, [], fn x, acc ->
         acc ++ [Enum.count(primeFactors, fn t -> t == x end)]
       end)
 
-    IO.puts("powersOnly: " <> "#{inspect(powersOnly)}")
+    # IO.puts("powersOnly: " <> "#{inspect(powersOnly)}")
     Enum.any?(powersOnly, fn power -> Integer.is_even(power) end)
   end
 
