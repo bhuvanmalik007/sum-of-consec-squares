ac8fe6e3f1b0b05220ebabe8764285c09c0c4eb3
bhuvan malik <bhuvanmalik007@gmail.com>
2018-09-09 18:21:14 -0400

    handled case when n<k

--- project.exs
+++ project.exs
@@ -1,6 +1,8 @@
 defmodule SupervisorModule do
   def supervisor(n, k) do
     # Total intervals are either k or k + 1
+    # singleProcessPID = n < k && spawn(SupervisorModule, :sumOfSquares, [%{start: 1, end: n}, k, self()])
+    # n < k && send singleProcessPID, []
     intervalListLength = trunc(n / k)
     totalIntervals = (rem(n, k) == 0 && k) || k + 1
 
@@ -13,7 +15,7 @@ defmodule SupervisorModule do
           Keyword.put_new([], :"interval#{inspect(intervalNo)}", %{start: startNo, end: endingNo})
       end)
 
-      # Adding remaining interval if totalIntervals = k + 1
+      # Adding remaining elements into another interval if totalIntervals = k + 1
       intervalList = (totalIntervals == ((k + 1)) &&
       (intervalList ++
         Keyword.put_new([], :"interval#{inspect(k + 1)}", %{start: ((k * trunc(n / k)) + 1), end: n}))) || intervalList
@@ -27,7 +29,7 @@ defmodule SupervisorModule do
         spawn(SupervisorModule, :sumOfSquares, [Keyword.get(intervalList, :"interval#{inspect(i)}"), k, acc])
       end) # end of reduce
 
-      # Initializing message passing from the last proces created
+      # Initializing message passing from the last process created
       send lastProcessPID, []
 
       # Receiving final result from the first process that was created
@@ -37,9 +39,10 @@ defmodule SupervisorModule do
       end
   end # end of supervisor
 
-  # This process computes a sequence within the interval range it was given
+  # This process computes a sequence within the interval range it was given and validates if the result is a perfect square
   def sumOfSquares(interval, k, next_pid) do
-    IO.puts("Running on bhuvan's machine")
+    # IO.puts("interval is #{inspect(interval)}")
+    # IO.puts("Running on bhuvan's machine")
     start = Map.get(interval, :start)
     finish = Map.get(interval, :end)
     result = Enum.reduce(start..finish, [], fn x, acc ->
@@ -50,17 +53,25 @@ defmodule SupervisorModule do
     # Adding result of this process to overall result
     receive do
       resultFromPrevious -> send next_pid, result ++ resultFromPrevious
+    end
   end
-end
 
-# Spawns the supervisor process
-def run(n, k) do
-  IO.puts inspect :timer.tc(SupervisorModule, :supervisor, [n, k])
-end
+  # Spawns the supervisor process
+  def run(n, k) do
+    if n < k do
+      final_answer = Enum.reduce(1..n, [], fn x, acc ->
+        sqRoot = :math.sqrt((k - 1) * k * (2 * k - 1) / 6 + k * (x * x) + k * (k - 1) * x)
+        (sqRoot - trunc(sqRoot) == 0 && acc ++ [x]) || acc
+        end)
+        (final_answer == [] && IO.puts("Result: No sequence")) || IO.puts("Result is #{inspect(final_answer)}")
+      else
+        IO.puts inspect :timer.tc(SupervisorModule, :supervisor, [n, k])
+    end
+  end
 
 end # SupervisorModule end
 
-# Handling if remote nodes are used or not. This enables us to to use the command "mix run "project.exs" arg1 arg2" is remote nodes are not being used.
+# Handling if remote nodes are used or not. This enables us to to use the command "mix run project.exs arg1 arg2" if remote nodes are not being used.
 if(!Node.alive?()) do
   {_, options, _} = OptionParser.parse(System.argv(), switches: [ help: :boolean],aliases: [ h: :help ])
   SupervisorModule.run(String.to_integer(Enum.at(options, 0)), String.to_integer(Enum.at(options, 1)))
